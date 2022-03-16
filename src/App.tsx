8b3e65b223902303d31d9ee054e8d3276a0aa43d
Bill Blanchette <wlblanchette@gmail.com>
2022-03-16 11:15:18 -0400

    API requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 // Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
 // Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import './App.css';
 import sampleData from './sample-data.json'
 import { ProductTable, Search } from './components'
+import { getResources } from './api'
+import { ProductApiResponse } from './types';
 
 function App() {
+  const [products, setProducts] = useState<ProductApiResponse>({
+    columns: [],
+    data: [],
+  })
+
+  // this feels messy, I would normally clean this up.
+  useLayoutEffect(() => {
+    if (!products.columns.length) {
+      getResources()
+        .then((response) => {
+          setProducts(response)
+        })
+    }
+  })
+
+  console.log('products', products)
   return (
     <div className="app">
       <header>
@@ -14,8 +32,8 @@ function App() {
       <main>
         <Search />
         <ProductTable
-          rows={sampleData.data}
-          cols={sampleData.columns}
+          rows={products.data}
+          cols={products.columns}
         />
       </main>
     </div>
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,15 @@
 // Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
 // Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
+import axios from 'axios'
+import { ProductApiResponse } from '../types'
+
 const API_URL = 'http://localhost:8080/resources'
-export {}
+
+const getResources = () => new Promise<ProductApiResponse>((res, rej) => {
+  axios
+    .get(API_URL)
+    .then((response) => res(response.data))
+    .catch(rej)
+})
+
+export { getResources }
